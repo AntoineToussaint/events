071dfcb6552b16c1daa5a8e604348ee0249b35cb
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-06-29 18:00:53 -0400

    impr: todo on the timeout of search

--- services/api/code/src/events/search.py
+++ services/api/code/src/events/search.py
@@ -3,6 +3,12 @@ from typing import List
 from src.models import EventSearchRequest, Event
 
 class Search(ABC):
+    """
+    Generic Search class
+
+    TODO: first improvement should be a non abstract class to handle time out
+    and use this one from the collector
+    """
     @abstractmethod
     async def get(self, request: EventSearchRequest) -> List[Event]:
         pass
